25ea5472c992ee9ce19cdd52d852416845b35cc5
Tavo <tavo@tavos-mbp.lan>
2025-09-12 11:02:37 -0700

    add sql & goose

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    CONSTRAINT UQ_name UNIQUE (name)
+);
+
+-- +goose Down
+DROP TABLE users;
